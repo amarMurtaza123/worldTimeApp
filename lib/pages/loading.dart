3d4e724626a298f8b3c0e3dafc80c8edfa9b9573
Amar Murtaza <amarMurtaza123>
2022-09-08 22:30:13 +0500

    World Time App

---
+++ lib/pages/loading.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+
+  void setupWorldTime() async{
+    WorldTime instance = WorldTime(location: 'Berlin', url: 'Europe/Berlin', flag: 'Germany.png');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home',arguments: {
+      'location': instance.location,
+      'time': instance.time,
+      'flag': instance.flag,
+      'isDayTime': instance.isDayTime,
+
+    });
+
+  }
+
+  @override
+  void initState() {
+
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
